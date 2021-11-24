33c7bfbcd451a4a1255a09de55934ef05ae15629
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-11-24 17:15:31 +0100

    commit 100% Oke

--- main.py
+++ main.py
@@ -12,25 +12,24 @@ class Player:
         self.speed = speed
         self.endurance = endurance
         self.accuracy = accuracy              
-        if speed > 1 or speed < 0:
+        if speed < 0 or speed > 1:
             raise ValueError (f'speed must be between 0 and 1')
-        if endurance > 1 or endurance < 0:
+        if endurance < 0 or endurance > 1:
             raise ValueError(f'endurence must be between 0 and 1')
-        if accuracy > 1 or accuracy < 0:
+        if accuracy < 0 or accuracy > 1:
             raise ValueError(f'accuracy must be between 0 and 1')
         
     def introduce(self):
-        return f'Hello everyone, my name is Super,{self.name}'
+        return f'Hello everyone, my name is {self.name}.'
 
 
     def strength(self):
-        if self.speed > self.endurance > self.accuracy:
-                return ('speed', self.speed)
-        elif self.endurance > self.accuracy:
-            return ('endurance', self.endurance)
+        if (self.speed >= self.endurance and self.speed >= self.accuracy):
+            return "speed", self.speed
+        elif (self.endurance > self.speed and self.endurance >= self.accuracy):
+            return "endurance", self.endurance
         else:
-            return ('accuracy', self.accuracy)   
- 
+            return "accuracy", self.accuracy
 #Opd part 2   
 
 class Commentator:
@@ -38,7 +37,10 @@ class Commentator:
         self.name = name
         
     def sum_player(self, player): 
-        return player.speed + player.endurance + player.accuracy
+        accuracy = getattr(player, "accuracy")
+        speed = getattr(player, "speed")
+        endurance = getattr(player, "endurance")
+        return speed + endurance + accuracy
     
     def compare_players(self, play_1, play_2, strength): 
         
@@ -50,16 +52,12 @@ class Commentator:
         elif player_one_strength < player_two_strength:
             return play_2.name
         elif play_1.strength()[1] > play_2.strength()[1]:
-            print('test')
             return play_1.name
         elif play_2.strength()[1] > play_1.strength()[1]:
-            print('test')
             return play_2.name
         elif self.sum_player(play_1) > self.sum_player(play_2):
-            print('test1')
             return play_1.name
         elif self.sum_player(play_2) > self.sum_player(play_1):
-            print('test1')
             return play_2.name
         else:
             return 'These two players might as well be twins!'
